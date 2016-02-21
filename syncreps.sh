a80d3123ce6eb6dea252d0a2a3c6fb844487b349
unknown <spichapp@BMC-4HQJMV1.adprod.bmc.com>
2016-02-21 05:19:00 +0000

    script to sync reps

---
+++ syncreps.sh
@@ -0,0 +1,13 @@
+cd ../
+git clone https://github.com/doodly-hackathon/mackenzie.git
+git clone https://github.com/doodly-hackathon/magna.git
+git clone https://github.com/doodly-hackathon/abscido.git
+git clone https://github.com/doodly-hackathon/simulata.git
+cd mackenzie 
+git pull
+cd ../magna
+git pull
+cd ../abscido
+git pull
+cd ../simulata
+git pull
